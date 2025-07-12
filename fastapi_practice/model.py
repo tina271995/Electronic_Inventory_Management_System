d549c7bba770935bc58af556e786fa9306c4c0a3
Kunal Sharma <kk.s67533@gmail.com>
2025-07-12 12:17:17 +0530

    Registration Completed

--- fastapi_practice/model.py
+++ fastapi_practice/model.py
@@ -1,19 +1,39 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+# from sqlalchemy import Column, Integer, String, ForeignKey,Boolean
+# from sqlalchemy.orm import relationship
+# from database import Base
+
+# class User(Base):
+#     __tablename__ = 'users'
+#     id = Column(Integer, primary_key=True, index=True)
+#     username = Column(String(50), unique=True, nullable=False)
+
+#     posts = relationship("Post", back_populates="user")
+
+# class Post(Base):
+#     __tablename__ = 'posts'
+#     id = Column(Integer, primary_key=True, index=True)
+#     title = Column(String(50), nullable=False)
+#     content = Column(String(100), nullable=False)
+#     user_Id = Column(Integer, ForeignKey('users.id'))
+#     user = relationship("User", back_populates="posts")
+
+
+from sqlalchemy import Column, DateTime, Integer, String, Boolean, ForeignKey, func
 from sqlalchemy.orm import relationship
 from database import Base
 
-class User(Base):
-    __tablename__ = 'users'
+class Registration(Base):
+    __tablename__ = "registration"  # fixed typo: resgistration → registration
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(50), unique=True, nullable=False)
+    Email = Column(String(200), nullable=False)
+    Password = Column(String(200), nullable=False)
+    Role = Column(Boolean, nullable=False)
+    logins = relationship("Login", back_populates="registration")
 
-    posts = relationship("Post", back_populates="user")
-
-class Post(Base):
-    __tablename__ = 'posts'
+class Login(Base):
+    __tablename__ = "login"
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(50), nullable=False)
-    content = Column(String(100), nullable=False)
-    user_Id = Column(Integer, ForeignKey('users.id'))
-
-    user = relationship("User", back_populates="posts")
+    RegistrationID = Column(Integer, ForeignKey("registration.id"), nullable=False)
+    LoginTimeStamp = Column(DateTime, nullable=False, default=func.now())
+    LoginStatus = Column(Boolean, nullable=False)
+    registration = relationship("Registration", back_populates="logins")
