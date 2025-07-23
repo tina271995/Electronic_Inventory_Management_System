855889c31021007e82f1368eef98e5ef1a4605bd
aakash21 <aakash@gmail.com>
2025-07-23 14:10:56 +0530

    minor changes

--- fastapi_practice/database.py
+++ fastapi_practice/database.py
@@ -3,8 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 
 
-
-DATABASE_URL = "mysql+pymysql://root:12345678@localhost/Tanvi1"
+DATABASE_URL = "mysql+pymysql://root:redhat@localhost/fastapi_db"
 engine = create_engine(DATABASE_URL, pool_size=10, max_overflow=30)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
